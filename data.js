177932a9333064ea9d25878f2b02ec08c286d107
Eytan Manor <eytan.manor@broadwing.io>
2022-01-02 09:52:15 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,25 @@
+let projects = {
+  "value of the project": [
+    ["<=200,000", "Low Risk", "AA", 7, 10, true],
+    [">200.000=<500.000", "Low to moderate", "A", 5.5, 7.9, true],
+    [">500.000=<1000.000", " moderate to risk", "B", 4, 5.7, true],
+    [">1000000=<3000000", "Moderate to High", "C", 2.5, 3.6, true],
+    [">3000000", "Extermely Hight", "D", 1, 1.4, true],
+  ],
+
+  "Type of the project": [
+    ["Governement", "Low risk", "A", 5.25, 7.5, true],
+    ["Comercial", "Low to moderate", "B", 3.25, 4.6, true],
+    ["Residental", "Moderate to High", "C", 1.15, 1.6, true],
+  ],
+  "Location of the project": [
+    ["<=50 km from Contractor", "Low risk", "A", 5.25, 7.5, true],
+    [">50km =< 120km from contractor", "Low to moderate", "B", 3.25, 4.6, true],
+    [">120 km from Contractor", "Moderate to High", "C", 1.15, 1.6, true],
+  ],
+
+  "Timeline of the project": [],
+  "Client GC/Project's owner Type": [],
+  "Client GC/Project's owner Size": [],
+  "Client GC/Project's owner Reputation": [],
+};
